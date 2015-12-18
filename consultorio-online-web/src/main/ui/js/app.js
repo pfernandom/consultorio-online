3aa6ee7e5f00d9ab14d928ff95bdf30d1b942dab
Pedro Marquez <pfernandom@gmail.com>
2015-12-17 23:30:48 -0600

    Merging projects into one single repository
    Combining the repositories consultorio-online and
    consultorio-online-web. Those projects are deprecated and will be
    deleted soon

---
+++ consultorio-online-web/src/main/ui/js/app.js
@@ -0,0 +1,48 @@
+var app = angular.module('main',['ui.bootstrap','formly', 'formlyBootstrap']);
+
+app.controller('MainCtrl',function(){
+	 var vm = this;
+
+  vm.user = {};
+
+  // note, these field types will need to be
+  // pre-defined. See the pre-built and custom templates
+  // http://docs.angular-formly.com/v6.4.0/docs/custom-templates
+  vm.userFields = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        type: 'email',
+        label: 'Email address',
+        placeholder: 'Enter email'
+      }
+    },
+    {
+      key: 'password',
+      type: 'input',
+      templateOptions: {
+        type: 'password',
+        label: 'Password',
+        placeholder: 'Password'
+      }
+    },
+    {
+      key: 'file',
+      type: 'file',
+      templateOptions: {
+        label: 'File input',
+        description: 'Example block-level help text here',
+        url: 'https://example.com/upload'
+      }
+    },
+    {
+      key: 'checked',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Check me out'
+      }
+    }
+  ];
+	
+});
